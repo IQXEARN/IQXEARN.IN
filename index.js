88eea959aea2735c9ce9d4e40085b5d8b7071291
IQXEARN <148423264+IQXEARN@users.noreply.github.com>
2023-10-19 11:00:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const loginsec=document.querySelector('.login-section')
+const loginlink=document.querySelector('.login-link')
+const registerlink=document.querySelector('.register-link')
+registerlink.addEventListener('click',()=>{
+    loginsec.classList.add('active')
+})
+loginlink.addEventListener('click',()=>{
+    loginsec.classList.remove('active')
+})
+const searchBarContainerEl = document.querySelector(".search-bar-container");
+
+const magnifierEl = document.querySelector(".magnifier");
+
+magnifierEl.addEventListener("click", () => {
+  searchBarContainerEl.classList.toggle("active");
+});
